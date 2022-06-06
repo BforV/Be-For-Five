614543956775d5bb64cf3dae1130f0020637363c
asilmolla <95424673+asilmolla@users.noreply.github.com>
2022-06-06 22:25:45 +0300

    Add files via upload

---
+++ BforV Scripts/CollectingCoins.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectingCoins : MonoBehaviour
+{
+    public AudioSource coinSound; 
+
+    void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Scoring.score += 1;
+            coinSound.Play();
+            Destroy(gameObject);
+        }
+    }
+}
