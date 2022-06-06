de3d59e2d7067ff32bc778958fa1b8a101cd5623
Halil Ibrahim Aygun <104322740+ravensho@users.noreply.github.com>
2022-06-06 23:30:23 +0300

    Add files via upload

---
+++ BforV Scripts/VolumeControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField] string _volumeParameter = "MasterVolume";
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider _slider;
+    [SerializeField] float _multiplier = 30f;
+    [SerializeField] private Toggle _toggle;
+    private void Awake()
+    {
+    _slider.onValueChanged.AddListener(HandleSliderValueChanged);
+    _toggle.onValueChanged.AddListener(HandleToggleValueChanged);
+    }
+    private void HandleToggleValueChanged(bool enableSound)
+    {
+        if (enableSound)
+            _slider.value = _slider.maxValue;
+        else
+            _slider.value = _slider.minValue;
+    }
+    private void OnDisable()
+    {
+    PlayerPrefs.SetFloat(_volumeParameter, _slider.value);
+    }
+    private void HandleSliderValueChanged(float value)
+    {
+       _mixer.SetFloat(_volumeParameter, value:Mathf.Log10(value)*_multiplier);
+       _toggle.isOn = _slider.value > _slider.minValue;
+    }
+    void Start()
+    {
+        _slider.value = PlayerPrefs.GetFloat(_volumeParameter, _slider.value);
+    }
+    void Update()
+    {
+        
+    }
+}
